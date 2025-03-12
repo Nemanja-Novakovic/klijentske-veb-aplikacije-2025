c0a3e447e736a3ff9dda7abb5e11f47bdeb003db
Nemanja-Novakovic <nemanja997novakovic@gmail.com>
2025-03-12 13:36:51 +0100

    cards for plain landing plan added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,12 @@ import { FlightService } from '../../services/flight.service';
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { AxiosError } from 'axios';
 import { FlightModel } from '../../models/flight.model';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe, NgIf, NgFor],
+  imports: [JsonPipe, NgIf, NgFor, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,7 +17,7 @@ export class HomeComponent {
   public error: string | null = null 
 
   constructor() {
-    FlightService.getFlights()
+    FlightService.getFlights(0,3)
       .then(rsp => this.flights = rsp.data.content)
       .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`)
   }
