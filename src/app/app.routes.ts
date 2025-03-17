872fd386f93a7678e162734e1565b2ba2c20a11b
Nemanja-Novakovic <nemanja997novakovic@gmail.com>
2025-03-17 13:16:17 +0100

    Add airlines page and loading page animation as service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import path from 'path';
+import { SearchComponent } from './search/search.component';
+import { AirlineComponent } from './airline/airline.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: '**', redirectTo: ''}
+    {path: 'search', component: SearchComponent},
+    {path: 'airlines', component:AirlineComponent},
+    {path: '**', redirectTo: ''} //bilo koja druga putanja ide na home page!
 ];
